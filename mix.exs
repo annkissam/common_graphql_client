b80fd4a9f837d244ce8c4cd6ac7c49930ec794d9
Joshua Adams <josh.adams@annkissam.com>
2018-05-31 13:58:13 -0400

    bumps version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CommonGraphqlClient.MixProject do
   def project do
     [
       app: :common_graphql_client,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
