4ac8756fdccbdd894e1653a6de0af5422cf2bfdb
marks <msebald@hhaexchange.com>
2023-05-09 12:56:16 -0500

    fix config.exs again

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,1 @@
 import Config
-
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-import_config "#{config_env()}.exs"
